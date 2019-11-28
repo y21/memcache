38a1e82a9d169b71cb4030d48a91f26455186c60
Timo <artur.fiedler54@gmail.com>
2019-11-28 22:42:21 +0100

    update functions

--- include/y21/memcache/Cache.h
+++ include/y21/memcache/Cache.h
@@ -3,6 +3,8 @@
 
 #include <map>
 #include <string>
+#include <stdexcept>
+#include <time.h>
 
 namespace memcache {
     template<typename T>
@@ -11,13 +13,28 @@ namespace memcache {
             std::map<std::string, T> internal_cache;
             uint16_t cacheLimit;
             bool ignoreQuery;
+            time_t expires;
         public:
-            Cache(uint16_t cacheLimit, bool ignoreQuery): cacheLimit(cacheLimit), ignoreQuery: ignoreQuery {
+            Cache(uint16_t cacheLimit, bool ignoreQuery, time_t expires): cacheLimit(cacheLimit), ignoreQuery(ignoreQuery), expires(expires) {
 
             }
 
             bool has(std::string key) {
-                return this->internal_cache::count(key) > 0;
+                return this->internal_cache.count(key) > 0;
+            }
+
+            T& add(std::string key, T val) {
+                if (this->cacheLimit == 0 || this->internal_cache.size() <= this->cacheLimit) {
+                    this->internal_cache[key] = val;
+                } else {
+                    throw new std::length_error("Cache limit exceeded");
+                }
+            }
+
+            static std::string removeQuery(std::string query) {
+                for (int i = 0; i < query.length(); ++i)
+                    if (query.at(i) == '?')
+                        return query.substr(0, i);
             }
     };
 }
